3f1ea7f63a1d8eb7c9f2c03f916fbb34e48c048b
Okonkwo Nwamaka <naomiparis938@gmail.com>
2023-04-24 05:40:54 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - prints the last digit if a randomly generated number
- *and whebever it is greater than 5, less than 6,or 0
+ * and whebever it is greater than 5, less than 6, or 0
  *
  * Return: Always 0
  */
