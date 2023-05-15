ea13f61288fc76c71e7b9c8c50aa2d00a03b6d61
Okonkwo Nwamaka <naomiparis938@gmail.com>
2023-05-15 15:30:10 -0700

    k

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,12 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to the stdout
  * @c: char to be printed
+ *
  * Return: 1
+ * on error, -1 is returned and error is set appropriately.
  */
 int _putchar(char c)
 {
